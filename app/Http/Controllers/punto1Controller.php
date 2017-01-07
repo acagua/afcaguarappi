2df21d7bc7d4efa1618affbd4d4f592de54799f6
acagua <af.cagua1011@gmail.com>
2017-01-07 16:33:16 -0500

    cambio index

---
+++ app/Http/Controllers/punto1Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class punto1Controller extends Controller
+{
+    //
+    function index()
+    {
+    	return view('punto1');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-
+/*
 Route::get('/', function () {
-    return view('welcome');
+    return view('punto1');
 });
+*/
+Route::get('/','punto1Controller@index');
