bc9dbdbbd925f1946c5433f11a9510260dfc225c
acagua <af.cagua1011@gmail.com>
2017-01-07 13:06:27 -0500

    cambio composer permisos directorios storage y bootsrap/cache

--- config/app.php
+++ config/app.php
@@ -100,10 +100,10 @@ return [
     | This key is used by the Illuminate encrypter service and should be set
     | to a random, 32 character string, otherwise these encrypted strings
     | will not be safe. Please do this before deploying an application!
-    |
+    | tk2AeCSffwBNSFL5nP6kQKfszvEWZe6KcUgePme5mP4=
     */
 
-    'key' => env('APP_KEY','tk2AeCSffwBNSFL5nP6kQKfszvEWZe6KcUgePme5mP4='),
+    'key' => env('APP_KEY’,’base64:sdzdi540if5/Ux8glGS2fu122sMic2+vu5QBsdxfiEg='),
 
     'cipher' => 'AES-256-CBC',
 
